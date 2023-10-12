367b047d8c07195b78381f3a4a9794878cf951f8
bhushan354 <deshmukhbhushan354@gmail.com>
2023-10-12 13:49:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
+import Home from './components.js/Home';
+import Hero from './components.js/Hero';
 
 function App() {
   return (
-    <h1>Hello appjs</h1>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/heroes/:heroId">Hero</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/heroes/:heroId" element={<Hero />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
